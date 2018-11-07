cd01e716f033dae91f285259b4dd05ba9788c545
Petr Salavec <petr.salavec@student.gyarab.cz>
2018-11-07 09:18:45 +0100

    Eh
    np, doesnt work yet

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 function solveExpression() {
   var vyraz = document.getElementById("vyraz").value;
-
-  document.getElementById("vysledek").innerHTML = "Výsledek je: " + vyraz;
+  let leva = 0;
+  let prava = 0;
+  var zmena = true;
+  for (let i = 0; i < vyraz.length; i++) {
+    if (vyraz.charAt(i) == "+") {
+      zmena = false;
+    } else if (vyraz.charAt(i) == "-") {
+      zmena = false;
+    } else if (vyraz.charAt(i) == "*") {
+      zmena = false;
+    } else if (vyraz.charAt(i) == "/") {
+      zmena = false;
+    } else {
+      if (zmena) {
+        leva = leva + parseInt(vyraz.charAt(i));
+      } else {
+        prava = prava + parseInt(vyraz.charAt(i));
+      }
+    }
+  }
+  document.getElementById("vysledek").innerHTML =
+    "Výsledek je: " + leva + " " + prava;
 }
 
 var input = document.getElementById("vyraz");
